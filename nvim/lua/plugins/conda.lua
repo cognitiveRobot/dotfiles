de2555ad1dcf54ea8a235c965da45ac37650c04c
Zulfikar <cognitiverobot@gmail.com>
2025-05-18 13:34:47 +1200

    doc added

--- nvim/lua/plugins/conda.lua
+++ nvim/lua/plugins/conda.lua
@@ -2,3 +2,9 @@ return {
 	"kmontocam/nvim-conda",
 	dependencies = { "nvim-lua/plenary.nvim" },
 }
+-- To show on the lualine add the function in lualine.lua
+-- then add env_name in the section where you want to show.
+-- local env_name = function()
+-- 	local name = vim.env.CONDA_DEFAULT_ENV
+-- 	return "(" .. name .. ")"
+-- end
