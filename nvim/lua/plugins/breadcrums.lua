36d60a2e84638f7b66e6064cf7c5cad003105220
Zulfikar <cognitiverobot@gmail.com>
2025-05-22 08:17:44 +1200

    clean up

--- nvim/lua/plugins/breadcrums.lua
+++ nvim/lua/plugins/breadcrums.lua
@@ -6,25 +6,6 @@ return {
 	},
 	config = function()
 		require("lspsaga").setup({})
-
 		vim.keymap.set("n", "<leader>ot", ":Lspsaga outline<CR>")
 	end,
 }
--- return {
---     "LunarVim/breadcrumbs.nvim",
---         dependencies = {
---             {
---                 "SmiteshP/nvim-navic",
---                 config = function()
---                     require("nvim-navic").setup {
---                         lsp = {
---                             auto_attach = true,
---                         },
---                     }
---                 end
---             },
---         },
---         config = function()
---             require("breadcrumbs").setup()
---         end
--- }
