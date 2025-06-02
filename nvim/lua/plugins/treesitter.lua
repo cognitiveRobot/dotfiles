8597e962eb51b4c95dc80c40ecf69931a2609bf3
Zulfikar <cognitiverobot@gmail.com>
2025-06-03 08:21:04 +1200

    autoformat + incremental selection

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,36 +1,36 @@
 return { -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  main = "nvim-treesitter.configs", -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = {
-      'lua',
-      'python',
-      'javascript',
-      'typescript',
-      'vimdoc',
-      'vim',
-      'regex',
-      'terraform',
-      'sql',
-      'dockerfile',
-      'toml',
-      'json',
-      'java',
-      'groovy',
-      'go',
-      'gitignore',
-      'graphql',
-      'yaml',
-      'make',
-      'cmake',
-      'markdown',
-      'markdown_inline',
-      'bash',
-      'tsx',
-      'css',
-      'html',
+      "lua",
+      "python",
+      "javascript",
+      "typescript",
+      "vimdoc",
+      "vim",
+      "regex",
+      "terraform",
+      "sql",
+      "dockerfile",
+      "toml",
+      "json",
+      "java",
+      "groovy",
+      "go",
+      "gitignore",
+      "graphql",
+      "yaml",
+      "make",
+      "cmake",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "tsx",
+      "css",
+      "html",
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
@@ -39,9 +39,18 @@ return { -- Highlight, edit, and navigate code
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
+      additional_vim_regex_highlighting = { "ruby" },
     },
-    indent = { enable = true, disable = { 'ruby' } },
+    indent = { enable = true, disable = { "ruby" } },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>", -- set to `false` to disable one of the mappings
+        node_incremental = "<CR>",
+        scope_incremental = false,
+        node_decremental = "<Backspace>",
+      },
+    },
   },
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
