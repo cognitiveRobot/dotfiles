f6d6e75292a3f041e8d1d1728b533af256eeee16
Zulfikar <cognitiverobot@gmail.com>
2025-08-22 18:33:59 +1200

    auto-commands to change window size

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -5,3 +5,24 @@ vim.api.nvim_create_autocmd({ "FocusGained" }, {
 	command = "checktime",
 	pattern = "*",
 })
+-- :W and :H to set win width/height
+vim.api.nvim_create_user_command("W", function(params)
+	local width = tonumber(params.fargs[1])
+	if not width then
+		return
+	end
+	if math.floor(width) ~= width then
+		width = math.floor(width * vim.o.columns)
+	end
+	vim.api.nvim_win_set_width(0, width)
+end, { nargs = 1 })
+vim.api.nvim_create_user_command("H", function(params)
+	local height = tonumber(params.fargs[1])
+	if not height then
+		return
+	end
+	if math.floor(height) ~= height then
+		height = math.floor(height * vim.o.lines - vim.o.cmdheight)
+	end
+	vim.api.nvim_win_set_height(0, height)
+end, { nargs = 1 })
