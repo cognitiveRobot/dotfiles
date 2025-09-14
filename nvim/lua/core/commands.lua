a21434eeddb36168dee70804aca1ac0607535971
Zulfikar <cognitiverobot@gmail.com>
2025-09-15 10:37:25 +1200

    navigation between terminal pane and nvim

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -5,6 +5,23 @@ vim.api.nvim_create_autocmd({ "FocusGained" }, {
 	command = "checktime",
 	pattern = "*",
 })
+local function set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+end
+
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+	pattern = { "*" },
+	callback = function(_)
+		vim.cmd.setlocal("nonumber")
+		vim.wo.signcolumn = "no"
+		set_terminal_keymaps()
+	end,
+})
 -- :W and :H to set win width/height
 vim.api.nvim_create_user_command("W", function(params)
 	local width = tonumber(params.fargs[1])
