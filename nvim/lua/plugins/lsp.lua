63b0e0d0875c7b56261085c326407e883e8a14bd
Zulfikar <cognitiverobot@gmail.com>
2025-10-06 19:03:28 +1300

    typescript setup - init

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -167,6 +167,8 @@ return {
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format Lua code
+			"tailwindcss-language-server",
+				"typescript-language-server",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
