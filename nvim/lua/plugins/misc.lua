222e38b5b1a78643072adbca97651feeec2ed067
Zulfikar <cognitiverobot@gmail.com>
2025-10-06 19:02:36 +1300

    misc plugins

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -28,6 +28,59 @@ return {
 		opts = {},
 	},
 	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{
+		"nvim-pack/nvim-spectre",
+		cmd = { "Spectre" },
+		-- dependencies = {
+		--   'nvim-lua/plenary.nvim',
+		-- },
+	},
+	{
+		"m4xshen/hardtime.nvim",
+		lazy = false,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {},
+	},
+	{
+		"tris203/precognition.nvim",
+		--event = "VeryLazy",
+		opts = {
+			-- startVisible = true,
+			-- showBlankVirtLine = true,
+			-- highlightColor = { link = "Comment" },
+			-- hints = {
+			--      Caret = { text = "^", prio = 2 },
+			--      Dollar = { text = "$", prio = 1 },
+			--      MatchingPair = { text = "%", prio = 5 },
+			--      Zero = { text = "0", prio = 1 },
+			--      w = { text = "w", prio = 10 },
+			--      b = { text = "b", prio = 9 },
+			--      e = { text = "e", prio = 8 },
+			--      W = { text = "W", prio = 7 },
+			--      B = { text = "B", prio = 6 },
+			--      E = { text = "E", prio = 5 },
+			-- },
+			-- gutterHints = {
+			--     G = { text = "G", prio = 10 },
+			--     gg = { text = "gg", prio = 9 },
+			--     PrevParagraph = { text = "{", prio = 8 },
+			--     NextParagraph = { text = "}", prio = 8 },
+			-- },
+			-- disabled_fts = {
+			--     "startify",
+			-- },
+		},
+	},
+	{
 		-- Highlight todo, notes, etc in comments
 		"folke/todo-comments.nvim",
 		event = "VimEnter",
