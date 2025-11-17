bb704ae17fd60b00eb1a157ddb5737b605f94790
yusuf <yusuf@fedora.fritz.box>
2025-11-18 06:17:52 +1300

    wezterm native split

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,9 +5,9 @@ local config = wezterm.config_builder()
 config = {
 	automatically_reload_config = true,
 	enable_tab_bar = false,
-	font_size = 16.0,
+	font_size = 14.0,
 	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE", -- disable title bar
+	window_decorations = "NONE", -- disable title bar
 	default_cursor_style = "BlinkingBar",
 
 	-- https://wezterm.org/config/appearance.html#defining-your-own-colors
@@ -21,6 +21,17 @@ config = {
 			key = "f",
 			mods = "CTRL",
 			action = wezterm.action.ToggleFullScreen,
+		},
+		{
+			key = "|",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+		{
+			key = "-",
+			mods = "SHIFT|CTRL",
+			action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
 		},
 		-- {
 		-- 	key = '\'',
